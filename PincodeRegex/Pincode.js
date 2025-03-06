409009628bd53dabaec00b0c2026d2bee3674aa5
Nitish singh <nitishkumarsingh877@gmail.com>
2025-03-06 12:07:19 +0530

    [Nitish] added first number as a nummber

--- PincodeRegex/Pincode.js
+++ PincodeRegex/Pincode.js
@@ -1,5 +1,5 @@
 const pinCode = Number(process.argv[2]);
-const pinCodeRegex = RegExp("[0-9]{6}")
+const pinCodeRegex = RegExp("[0-9][0-9]{5}")
 
 if(pinCodeRegex.test(pinCode)){
   console.log("Pin code is valid !!!")
